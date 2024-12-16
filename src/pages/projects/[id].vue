bc5bd746b852ee3769b82f0b1c9594ddebfd8c62
jeric <jericepon@gmail.com>
2024-12-16 09:40:52 +0800

    add initial Vue 3 setup with routing and basic pages

---
+++ src/pages/projects/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = ref((route.params as { id: string }).id);
+</script>
+<template>
+  <div>
+    <h1>Single Project View {{ id }}</h1>
+    <router-link to="/projects">Go back to projects</router-link>
+  </div>
+</template>
+
+<style scoped lang='scss'></style>
---
+++ src/pages/projects/index.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+</script>
+<template>
+  <div>
+    <h1>Projects View</h1>
+    <router-link to="/">Go back to home</router-link>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+</style>

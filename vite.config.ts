5517eccb454fd9a809d71be27150c60713091709
jeric <jericepon@gmail.com>
2024-12-16 09:40:52 +0800

    add initial Vue 3 setup with routing and basic pages
    (cherry picked from commit bc5bd746b852ee3769b82f0b1c9594ddebfd8c62)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    VueRouter(),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
